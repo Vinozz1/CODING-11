197cda32d4b1de573900ccdb60851779f1f2be65
Jonathan Alvino <jonathanalvino@Jonathans-MacBook-Pro.local>
2024-08-23 22:13:45 +0700

    Mid test 1

---
+++ AP COMP/03/Expressions.java
@@ -0,0 +1,6 @@
+public class Expressions {
+    public static void main (String[] args)  {
+        System.out.println(14 / 5 + " " + -3 / 2 );
+        System.out.println(45 % 6 + " " + 2 % 2 );
+    }
+}
